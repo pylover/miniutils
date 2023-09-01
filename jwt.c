e713853dd661ba685d3ba8ab1fcd850586e330a1
Hossein Sokhandan <iamsokhandan@gmail.com>
2023-09-01 23:24:27 +0330

    Implement JWT helpers generate/verify

--- jwt.c
+++ jwt.c
@@ -1,176 +1,222 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <openssl/bio.h>
-#include <openssl/evp.h>
-#include <openssl/buffer.h>
 #include <openssl/hmac.h>
-#include <clog.h>
+#include <openssl/buffer.h>
 
 
-#define BASE64_MAXLEN 128
-#define TOKENMAXLEN BASE64_MAXLEN * 3
+#define BEARER_TOKEN_SIZE 128 
+#define HMAC_LEN 32
+#define HMAC_ENCODEDLEN (((HMAC_LEN + 2) / 3) * 4)
+
+
+static const char base64url_alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghi"
+        "jklmnopqrstuvwxyz0123456789-_";
 
 
 int
-base64url_encode(const unsigned char *decoded, char *encoded,
-        size_t encoded_length) {
-    BIO* b64 = BIO_new(BIO_f_base64());
-    BIO* bio = BIO_new(BIO_s_mem());
-    bio = BIO_push(b64, bio);
+base64url_encode(const unsigned char *input, size_t input_length,
+        char *output) {
+    /* Create a BIO object for Base64 encoding */
+    BIO *bio = BIO_new(BIO_f_base64());
+    if (bio == NULL) {
+        return -1;
+    }
 
+    /* Set the BIO flags to use URL-safe Base64 encoding */
     BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL);
-    BIO_write(bio, decoded, strlen(decoded));
+
+    /* Create a memory BIO to write the output */
+    BIO *mem_bio = BIO_new(BIO_s_mem());
+    if (mem_bio == NULL) {
+        BIO_free_all(bio);
+        return -1;
+    }
+
+    /* Chain the memory BIO to the Base64 encoding BIO */
+    bio = BIO_push(bio, mem_bio);
+
+    /* Write the input data to the BIO */
+    BIO_write(bio, input, input_length);
     BIO_flush(bio);
 
-    BUF_MEM* buffer_ptr;
-    BIO_get_mem_ptr(bio, &buffer_ptr);
+    /* Retrieve the encoded output from the memory BIO */
+    BUF_MEM *mem_buf;
+    BIO_get_mem_ptr(bio, &mem_buf);
 
-    /* Replace base64 characters with base64url characters */
+    /* Convert the standard Base64 alphabet to Base64URL alphabet */
     size_t i;
-    for (i = 0; i < buffer_ptr->length; i++) {
-        if (buffer_ptr->data[i] == '+') {
-            buffer_ptr->data[i] = '-';
-        }
-        else if (buffer_ptr->data[i] == '/') {
-            buffer_ptr->data[i] = '_';
-        }
-        else if (buffer_ptr->data[i] == '=') {
-            buffer_ptr->data[i] = '\0';
+    for (i = 0; i < mem_buf->length; i++) {
+        if (mem_buf->data[i] == '+') {
+            output[i] = '-';
+        } else if (mem_buf->data[i] == '/') {
+            output[i] = '_';
+        } else if (mem_buf->data[i] == '=') {
+            /* Padding characters; stop here */
+            break;
+        } else {
+            output[i] = mem_buf->data[i];
         }
     }
-    
-    
-    if (encoded_length < buffer_ptr->length) {
-        return -1;
-    }
+    output[i] = '\0';
 
-    memcpy(encoded, buffer_ptr->data, buffer_ptr->length);
+    /* Clean up the BIO objects */
     BIO_free_all(bio);
-    return 0;
-}
-
-
-static const char b64url_table[1024] = {
-    ['A']= 0, ['B']= 1, ['C']= 2, ['D']= 3, ['E']= 4, ['F']= 5, ['G']= 6,
-    ['H']= 7, ['I']= 8, ['J']= 9, ['K']=10, ['L']=11, ['M']=12, ['N']=13,
-    ['O']=14, ['P']=15, ['Q']=16, ['R']=17, ['S']=18, ['T']=19, ['U']=20,
-    ['V']=21, ['W']=22, ['X']=23, ['Y']=24, ['Z']=25, ['a']=26, ['b']=27,
-    ['c']=28, ['d']=29, ['e']=30, ['f']=31, ['g']=32, ['h']=33, ['i']=34,
-    ['j']=35, ['k']=36, ['l']=37, ['m']=38, ['n']=39, ['o']=40, ['p']=41,
-    ['q']=42, ['r']=43, ['s']=44, ['t']=45, ['u']=46, ['v']=47, ['w']=48,
-    ['x']=49, ['y']=50, ['z']=51, ['0']=52, ['1']=53, ['2']=54, ['3']=55,
-    ['4']=56, ['5']=57, ['6']=58, ['7']=59, ['8']=60, ['9']=61, ['-']=62,
-    ['_']=63
-};
-
-
-int
-base64url_decode(const char *input, size_t input_length,
-        unsigned char *output) {
-    size_t padding = 0;
-    if (input_length % 4 != 0) {
-        padding = 4 - (input_length % 4);
-    }
-
-    size_t decoded_length = ((input_length + padding) / 4) * 3;
-
-    // Adjust the decoded length for padding
-    if (input[input_length - 1] == '=') {
-        decoded_length--;
-        if (input[input_length - 2] == '=') {
-            decoded_length--;
-        }
-    }
-
-    if (input_length < decoded_length) {
-        printf("dddd");
-        return -1; // Output buffer too small
-    }
-
-    size_t i, j;
-    for (i = 0, j = 0; i < input_length; i += 4, j += 3) {
-        int n = (b64url_table[(unsigned char) input[i]] << 18) |
-                (b64url_table[(unsigned char) input[i + 1]] << 12) |
-                (b64url_table[(unsigned char) input[i + 2]] << 6) |
-                b64url_table[(unsigned char) input[i + 3]];
-
-        output[j] = (unsigned char) ((n >> 16) & 0xFF);
-        if (j + 1 < decoded_length) {
-            output[j + 1] = (unsigned char) ((n >> 8) & 0xFF);
-        }
-        if (j + 2 < decoded_length) {
-            output[j + 2] = (unsigned char) (n & 0xFF);
-        }
-    }
-    output[input_length] = '\0';
 
     return 0;
 }
 
 
 int
-jwt_generate(const unsigned char *payload, const char *secret, char *token,
-        size_t token_length) {
-    /* In this version caller cannot provide header. */
-    const unsigned char *header = "{\"alg\": \"HS256\", \"typ\": \"JWT\"}";
-    char *encodedheader;
-    char *encodedpayload;
-    char *encodedsignature;
-    unsigned char signature[EVP_MAX_MD_SIZE];
+base64url_decode(char *input, size_t input_length, unsigned char *output) {
+    /* Convert the Base64URL alphabet to the standard Base64 alphabet */
+    size_t i;
+    for (i = 0; i < input_length; i++) {
+        if (input[i] == '-') {
+            input[i] = '+';
+        } else if (input[i] == '_') {
+            input[i] = '/';
+        }
+    }
 
-    encodedheader = malloc(BASE64_MAXLEN);
-    base64url_encode(header, encodedheader, BASE64_MAXLEN);
-
-    encodedpayload = malloc(BASE64_MAXLEN);
-    base64url_encode(payload, encodedpayload, BASE64_MAXLEN);
-
-    /* +4 for the dots and null-terminator. */
-    size_t token_maxlen = strlen(encodedheader) + strlen(encodedpayload) +
-            strlen(secret) + 4;
-    if (token_length < token_maxlen) {
-        /* Insufficient token buffer size. */
+    /* Create a BIO object for Base64 decoding */
+    BIO *bio = BIO_new(BIO_f_base64());
+    if (bio == NULL) {
         return -1;
     }
-    
-    snprintf(token, token_length, "%s.%s.", encodedheader, encodedpayload);
 
-    HMAC(EVP_sha256(), secret, strlen(secret), (unsigned char*)token,
-            strlen(token), signature, NULL);
+    /* Set the BIO flags for URL-safe Base64 decoding */
+    BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL);
 
-    encodedsignature = malloc(BASE64_MAXLEN);
-    base64url_encode(signature, encodedsignature, BASE64_MAXLEN);
-    strcat(token, encodedsignature);
+    /* Create a memory BIO to read the input */
+    BIO *mem_bio = BIO_new_mem_buf(input, input_length);
+    if (mem_bio == NULL) {
+        BIO_free_all(bio);
+        return -1;
+    }
 
-    free(encodedheader);
-    free(encodedpayload);
-    free(encodedsignature);
+    /* Chain the memory BIO to the Base64 decoding BIO */
+    bio = BIO_push(bio, mem_bio);
+
+    /* Read the input and decode into the output buffer */
+    int output_length = BIO_read(bio, output, input_length);
+    if (output_length < 0) {
+        BIO_free_all(bio);
+        return -1;
+    }
+
+    /* Clean up the BIO objects */
+    BIO_free_all(bio);
+
+    return output_length;
+}
+
+
+void
+calculate_hmac_sha256(const unsigned char *data, int data_len,
+        const unsigned char *key, int key_len, unsigned char *result) {
+    unsigned int len = HMAC_LEN;
+    HMAC(EVP_sha256(), key, key_len, data, data_len, result, &len);
+}
+
+
+int
+jwt_generate(const char *payload, const char *secret, char *token) {
+    /* Fixed header is only supported in this version */
+    const char *header = "{\"alg\":\"HS256\",\"typ\":\"JWT\"}";
+    size_t header_length = strlen(header);
+    char encoded_header[BEARER_TOKEN_SIZE];
+    base64url_encode((const unsigned char *)header, header_length,
+                     encoded_header);
+
+    size_t payload_length = strlen(payload);
+    char encoded_payload[BEARER_TOKEN_SIZE];
+    base64url_encode((const unsigned char *)payload, payload_length,
+                     encoded_payload);
+
+    /* Concatenate header and payload with a period ('.') separator */
+    char data[BEARER_TOKEN_SIZE * 2 + 2];
+    sprintf(data, "%s.%s", encoded_header, encoded_payload);
+
+    unsigned char hmac[HMAC_LEN];
+    unsigned int hmac_length;
+    HMAC(EVP_sha256(), secret, strlen(secret), (const unsigned char *)data,
+         strlen(data), hmac, &hmac_length);
+
+    char encoded_signature[BEARER_TOKEN_SIZE];
+    base64url_encode(hmac, hmac_length, encoded_signature);
+
+    sprintf(token, "%s.%s.%s", encoded_header, encoded_payload,
+            encoded_signature);
 
     return 0;
 }
 
 
-int main() {
-    const char *input = "eyJ1c2VyX2lkIjogMTMsICJpYXQiOiAxNTE2MjM5MDIyfQ";
-    size_t input_length = strlen(input);
+int
+jwt_verify(char *token, char *secret) {
+    char *header;
+    char *payload;
+    char *signature;
+    char *saveptr;
 
-    // Calculate the maximum possible size for the decoded output
-    size_t max_output_length = (input_length / 4) * 3;
+    /* Split the token into header, payload, and signature */
+    char *part = strtok_r(token, ".", &saveptr);
+    int i;
+    for (i = 0; part != NULL; ++i) {
+        if (i == 0) {
+            header = part;
+        }
+        else if (i == 1) {
+            payload = part;
+        }
+        else if (i == 2) {
+            signature = part;
+        }
+        part = strtok_r(NULL, ".", &saveptr);
+    }
 
-    // Allocate a buffer to store the decoded output (add 1 for null-terminator)
-    unsigned char output[max_output_length + 1];
+    /* Concatenate header and payload with a period separator */
+    char msg[BEARER_TOKEN_SIZE];
+    strcpy(msg, header);
+    strcat(msg, ".");
+    strcat(msg, payload);
 
-    // Decode the Base64URL-encoded input
-    int result = base64url_decode(input, input_length, output);
+    /* Calculate the HMAC SHA256 */
+    unsigned char hmac[HMAC_LEN];
+    calculate_hmac_sha256((unsigned char*)msg, strlen(msg),
+            (unsigned char*)secret, strlen(secret), hmac);
 
-    if (result == 0) {
-        // Null-terminate the decoded output
+    /* Base64url encode the HMAC */
+    char encoded_hmac[HMAC_ENCODEDLEN];
+    base64url_encode(hmac, sizeof(hmac), encoded_hmac);
 
-        // Print the decoded output
-        printf("Decoded output: %s\n", output);
-    } else {
-        printf("Decoding failed.\n");
+    /* Compare the provided signature with the calculated one */
+    if (strcmp(signature, encoded_hmac) != 0) {
+        return -1;
     }
 
     return 0;
 }
+
+
+int
+main() {
+    char payload[] = "{\"user\":\"admin\",\"iat\":1422779638}";
+    char jwt[BEARER_TOKEN_SIZE];
+    char secret[] = "7{yX5OB_zh?Hv]|5PO`H:Jn?Z=LNaB^_";
+
+    if (jwt_generate(payload, secret, jwt) != 0) {
+        printf("JWT generation failed.\n");
+        return -1;
+    }
+
+    printf("Generated JWT: %s\n", jwt);
+
+    if (jwt_verify(jwt, "7{yX5OB_zh?Hv]|5PO`H:Jn?Z=LNaB^_") != 0) {
+        printf("JWT verification failed.\n");
+        return -1;
+    }
+
+    printf("JWT verification succeeded.\n");
+    return 0;
+}
+
